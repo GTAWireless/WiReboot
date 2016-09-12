6983c4e76e3f8267fb95bda01882dee44b84741e
Mike <mikewen@gmail.com>
2016-09-11 23:57:40 -0400

    add download module

--- Lua/d.lua
+++ Lua/d.lua
@@ -2,54 +2,48 @@ local moduleName = ...
 local M = {}
 _G[moduleName] = M
 
-host="d.wireboot.com"
+-- url="http://ota.wireboot.com/cm/"
+function M.g(url,f,c)
+  tf,hex=nil,nil
 
-function M.g(p,f,a,c)
-  local strPost = "GET /"..p.."f="..f.." HTTP/1.0\r\nHost: "..host.."\r\n\r\n"
-  local sk=net.createConnection(net.TCP, 0)
-
-  local s=0
-
-  sk:on("connection", function(sck) sk:send(strPost) end)
-
-  sk:on("receive", function(sck, res)
-  --print(res)
-     local pos=string.find(res,"%c%c%c")
-     --print(pos)
-     if pos~=nil and res:len()>100 then
-          s=1
-          pos=pos+4
-     else
-        pos=0
-     end
-     if s==1 then
-        if a==nil then
-          file.remove(f)
-        end
-        file.open(f,"a")
-
-        file.write(res:sub(pos))
+  http.get(url, nil, function(code, data)
+    if (code<0) then
+        return
+    else
+        tf='_'..f
+        file.open(tf,"w") --temp file
+        file.write(data)
         file.close()
+        data=nil
+        
+        hex=crypto.toHex(crypto.fhash("md5",tf))
+
         if c~=nil then
            node.input(c)
         end
-     end
-     res=nil
-     collectgarbage()
+
+        tmr.alarm(3,500,0,function()
+            --print(tf,hex)
+              http.get(url..'.md5', nil, function(code, data)
+                if (code<0) then
+                    --print('err:md5')
+                    return
+                else
+                    --print(hex,data)
+                    if data==hex then
+                      file.remove(f)
+                      file.rename(tf,f)
+                    end
+                    data=nil
+                end
+            end)
+            -- collectgarbage() 
+          end)
+
+    end
   end)
-
-  sk:dns(host,function(conn,ip) sk:connect(80,ip) end)
-  tmr.alarm(5,2000,0,function() sk:close() sk=nil strPost = nil file.close() collectgarbage() end)
-end
-
-function M.r(ip,mac)
- local strPost = "GET /a?ip="..ip.."&mac="..mac.." HTTP/1.0\r\nHost: "..host.."\r\n\r\n"
- local sk=net.createConnection(net.TCP, 0)
-
- sk:on("connection", function(sck) sk:send(strPost) end)
- sk:on("receive", function(sck, res) print(res) res=nil collectgarbage() end)
- sk:dns(host,function(conn,ip) sk:connect(80,ip) print(ip) collectgarbage() end)
- tmr.alarm(5,2000,0,function() sk:close() sk=nil strPost = nil collectgarbage() end)
+  
+  --tmr.alarm(5,2000,0,function() sk:close() sk=nil strPost = nil file.close() collectgarbage() end)
 end
 
 return M
